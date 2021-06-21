653b31ca9a739cee67d45c8d3ab7a974c025b1ab
unknown <Uhnder@UHIN-LT-108.uhnder.com>
2021-06-21 18:32:42 +0530

    Completed Huffman Assignment

--- test.py
+++ test.py
@@ -8,11 +8,27 @@ class TestHuffman(unittest.TestCase):
 	# function name should be prefixed with 'test'
 
 	def test_encode(self):
-		encode("", "")
+		encode("story.txt", "story.huff")
 		assert True
 
 	def test_decode(self):
-		decode("", "")
+		decode("story.huff", "story_.txt")
+		assert True
+
+	def test_encode(self):
+		encode("story_1.txt", "story_1.huff")
+		assert True
+
+	def test_decode(self):
+		decode("story_1.huff", "story_1_.txt")
+		assert True
+
+	def test_encode(self):
+		encode("story_2.txt", "story_2.huff")
+		assert True
+
+	def test_decode(self):
+		decode("story_2.huff", "story_2_.txt")
 		assert True
 
 
