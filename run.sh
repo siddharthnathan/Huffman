635ca6c62b9ab1df51c88ea60cd6f9a496884f46
Siddharth <45594964+siddharthnathan@users.noreply.github.com>
2021-06-25 12:00:27 +0530

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,10 +1,44 @@
 #!/usr/bin/env bash
-# encode story.txt
+
+# Encode story.txt
 time python3 huffman.py -e story.txt -o story.huff
-# decode story.txt
+
+# Decode story.txt
 time python3 huffman.py -d story.huff -o story_.txt
-# find differences
+
+# Find Differences
 ./diff.sh story.txt story_.txt
-#find compression ratio
-echo "Compression ratio"
+
+#Find Compression Ratio
+echo "Compression Ratio"
 expr $(ls -l story.txt | awk '{ print $5 }')/$(ls -l story.huff | awk '{ print $5 }') | bc -l
+
+
+# Encode story_1.txt
+time python3 huffman.py -e story_1.txt -o story_1.huff
+
+# Decode story_1.txt
+time python3 huffman.py -d story_1.huff -o story_1_.txt
+
+# Find Differences
+./diff.sh story_1.txt story_1_.txt
+
+#Find Compression Ratio
+echo "Compression Ratio"
+expr $(ls -l story_1.txt | awk '{ print $5 }')/$(ls -l story_1.huff | awk '{ print $5 }') | bc -l
+
+
+# Encode story_2.txt
+time python3 huffman.py -e story_2.txt -o story_2.huff
+
+# Decode story_2.txt
+time python3 huffman.py -d story_2.huff -o story_2_.txt
+
+# Find Differences
+./diff.sh story_2.txt story_2_.txt
+
+#Find Compression Ratio
+echo "Compression Ratio"
+expr $(ls -l story_2.txt | awk '{ print $5 }')/$(ls -l story_2.huff | awk '{ print $5 }') | bc -l
+
+
